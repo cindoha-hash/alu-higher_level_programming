8a91df4b1e7540018db69e585bf590a5f763e42a
cindoha-hash <c.indoha@alustudent.com>
2026-04-02 15:32:24 -0700

    swqat

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
--- Creates user_0d_1 with all privileges
+-- Creates user_0d_1 and grants all privileges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
