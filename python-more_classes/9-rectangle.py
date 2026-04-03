551521c043ece20b3b4bdb1da07d42ce97f4a5dd
none <none@none.com>
2026-04-03 21:53:44 +0000

    ff

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -26,9 +26,13 @@ class Rectangle:
     def width(self, value):
         """Set width"""
         if type(value) is not int:
-            raise TypeError("width must be an integer")
+            raise TypeError(
+                "width must be an integer"
+            )
         if value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError(
+                "width must be >= 0"
+            )
         self.__width = value
 
     @property
@@ -40,9 +44,13 @@ class Rectangle:
     def height(self, value):
         """Set height"""
         if type(value) is not int:
-            raise TypeError("height must be an integer")
+            raise TypeError(
+                "height must be an integer"
+            )
         if value < 0:
-            raise ValueError("height must be >= 0")
+            raise ValueError(
+                "height must be >= 0"
+            )
         self.__height = value
 
     def area(self):
@@ -53,7 +61,9 @@ class Rectangle:
         """Return rectangle perimeter"""
         if self.__width == 0 or self.__height == 0:
             return 0
-        return 2 * (self.__width + self.__height)
+        return 2 * (
+            self.__width + self.__height
+        )
 
     def __str__(self):
         """Return printable rectangle"""
@@ -71,7 +81,8 @@ class Rectangle:
     def __repr__(self):
         """Return recreatable rectangle"""
         return "Rectangle({}, {})".format(
-            self.__width, self.__height
+            self.__width,
+            self.__height
         )
 
     @staticmethod
@@ -92,5 +103,5 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
-        """Return a square rectangle"""
+        """Return a square"""
         return cls(size, size)
